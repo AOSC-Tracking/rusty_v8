a47beb1fe1cf4644e1eae6c37122500f02616261
CAB233 <yidaduizuoye@outlook.com>
2026-01-29 23:09:43 +0800

    AOSCOS: Disable cross-compilation setup on arm64
    Because we have native linux arm64 CI :)

--- build.rs
+++ build.rs
@@ -357,14 +357,14 @@ fn build_v8(is_asan: bool) {
     }
   }
   // cross-compilation setup
-  if target_arch == "aarch64" {
-    gn_args.push(r#"target_cpu="arm64""#.to_string());
-    if target_os == "linux" {
-      gn_args.push("use_sysroot=true".to_string());
-      maybe_install_sysroot("arm64");
-      maybe_install_sysroot("amd64");
-    }
-  }
+  //if target_arch == "aarch64" {
+  //  gn_args.push(r#"target_cpu="arm64""#.to_string());
+  //  if target_os == "linux" {
+  //    gn_args.push("use_sysroot=true".to_string());
+  //    maybe_install_sysroot("arm64");
+  //    maybe_install_sysroot("amd64");
+  //  }
+  //}
   if target_arch == "arm" {
     gn_args.push(r#"target_cpu="arm""#.to_string());
     gn_args.push(r#"v8_target_cpu="arm""#.to_string());
